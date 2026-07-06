828264d736f8e2a84f99857187fae4c0b0159fec
junbl <bloomjune@pm.me>
2026-07-05 22:02:21 -0600

    added options on rolls with thorns to have crits not ignore thorns and to customize crit probability separate from perfect

--- src/dice/analysis/RollsWithThornsAnalysis.tsx
+++ src/dice/analysis/RollsWithThornsAnalysis.tsx
@@ -28,6 +28,7 @@ import { colors } from "../../theme";
 import { proportionFormat } from "./DiminishingPoolsAnalysis";
 import binomcoef from "@stdlib/math-base-special-binomcoef";
 import { parseFraction } from ".";
+import { d } from "../crucible/Crucible";
 
 const data: Record<string, Record<string, (typeof rollsData)["1"]["0"]>> = rollsData;
 const barColors: Record<string, string> = {
@@ -41,17 +42,20 @@ const barColors: Record<string, string> = {
 
 const defaultGrim = 1 / 2;
 const defaultPerfect = 1 / 6;
+const defaultCrit = defaultPerfect;
 const defaultCut = 2 / 8;
 
 function pdt(
     dice: number,
     thorns: number,
     mastery: number,
-    remap: Record<string, number | undefined> = {}
+    critsIgnoreThorns: boolean,
+    remap: Record<string, number | undefined> = {},
 ) {
     const get_remap = (field: string, fallback: number) => remap[field] || fallback;
     const p_g1 = get_remap("grim", defaultGrim);
     const p_p1 = get_remap("perfect", defaultPerfect);
+    const p_c = get_remap("critical", p_p1);
 
     if (p_g1 + p_p1 > 1) {
         console.error("invalid prob");
@@ -62,17 +66,26 @@ function pdt(
 
     const q = (p: number) => 1 - p;
     const q_p1 = q(p_p1);
+    const q_c = q(p_c);
 
-    const grim = p_g1 ** (dice + mastery);
-    const messy = 1 - (p_g1 ** (dice + mastery) + (1 - q_p1 ** (dice + mastery)));
-    const perfect = dice * p_p1 * q_p1 ** (dice - 1 + mastery);
+    const allDice = dice + mastery;
+
+    const grim = p_g1 ** allDice;
+    const messy = 1 - (p_g1 ** allDice + (1 - q_p1 ** allDice));
+
+    // either you got a non-crit perfect on as many dice as you want,
+    // or you got exactly one crit perfect and no more
     const critical =
-        mastery * p_p1 * q_p1 ** (mastery - 1 + dice) +
-        (1 - (q_p1 ** dice + dice * p_p1 * q_p1 ** (dice - 1))) * q_p1 ** mastery;
+        mastery * p_c * q_c ** (mastery - 1 + dice) +
+        (1 - (q_c ** dice + dice * p_c * q_c ** (dice - 1))) * q_c ** mastery;
     const doubleCritical =
         1 -
-        (q_p1 ** mastery + mastery * p_p1 * q_p1 ** (mastery - 1)) +
-        mastery * p_p1 * q_p1 ** (mastery - 1) * (1 - q_p1 ** dice);
+        (q_c ** mastery + mastery * p_c * q_c ** (mastery - 1)) +
+        mastery * p_c * q_c ** (mastery - 1) * (1 - q_c ** dice);
+
+    const anyCritical = critical + doubleCritical;
+
+    const perfect = q(q_p1 ** allDice) - anyCritical;
 
     const totalPreThorns = grim + messy + perfect + critical + doubleCritical;
     if (Math.abs(totalPreThorns - 1) > 0.001) {
@@ -90,15 +103,39 @@ function pdt(
     const p_cuts = (cuts: number) =>
         binomcoef(thorns, cuts) * p_t1 ** cuts * q(p_t1) ** (thorns - cuts);
 
-    const p_disaster =
+    let p_disaster =
         grim * (1 - p_cuts(0)) +
         messy * (1 - (p_cuts(0) + p_cuts(1))) +
         perfect * (1 - (p_cuts(0) + p_cuts(1) + p_cuts(2)));
-    const p_grim = grim * p_cuts(0) + messy * p_cuts(1) + perfect * p_cuts(2);
-    const p_messy = messy * p_cuts(0) + perfect * p_cuts(1);
-    const p_perfect = perfect * p_cuts(0);
-    const p_critical = critical;
-    const p_doubleCritical = doubleCritical;
+    let p_grim = grim * p_cuts(0) + messy * p_cuts(1) + perfect * p_cuts(2);
+    let p_messy = messy * p_cuts(0) + perfect * p_cuts(1);
+    let p_perfect = perfect * p_cuts(0);
+    let p_critical = critical;
+    let p_doubleCritical = doubleCritical;
+
+    if (!critsIgnoreThorns) {
+
+        p_disaster =
+            grim * (1 - p_cuts(0))
+            + messy * (1 - (p_cuts(0) + p_cuts(1)))
+            + perfect * (1 - (p_cuts(0) + p_cuts(1) + p_cuts(2)))
+            + critical * (1 - (p_cuts(0) + p_cuts(1) + p_cuts(2)) + p_cuts(3))
+            + doubleCritical * (1 - (p_cuts(0) + p_cuts(1) + p_cuts(2)) + p_cuts(3));
+        p_grim =
+            grim * p_cuts(0)
+            + messy * p_cuts(1)
+            + perfect * p_cuts(2)
+            + anyCritical * p_cuts(3);
+        p_messy =
+            messy * p_cuts(0)
+            + perfect * p_cuts(1)
+            + anyCritical * p_cuts(2);
+        p_perfect = perfect * p_cuts(0) + (anyCritical * p_cuts(1));
+
+        p_critical = critical * p_cuts(0);
+        p_doubleCritical = doubleCritical * p_cuts(0);
+
+    }
 
     const total_post_thorns = p_grim + p_messy + p_perfect + p_critical + p_doubleCritical;
     if (Math.abs(totalPreThorns - 1) > 0.001) {
@@ -182,14 +219,17 @@ export default function RollsWithThornsAnalysis() {
     const theme = useTheme();
     const mobileScreen = useMediaQuery(theme.breakpoints.between("xs", "sm"));
 
-    const [selectedNumDice, setSelectedNumDice] = useState("3");
-    const [selectedNumThorns, setSelectedNumThorns] = useState("1");
+
+    const [selectedNumDice, setSelectedNumDice] = useState((1 + d(5)).toString());
+    const [selectedNumThorns, setSelectedNumThorns] = useState(d(4).toString());
     const [selectedNumMastery, setSelectedNumMastery] = useState("0");
     const [settingsOpen, setSettingsOpen] = useState(false);
 
     const [fivesAsSixesFoursAsOnes, setFivesAsSixesFoursAsOnes] = useState(false);
+    const [critsIgnoreThorns, setCritsIgnoreThorns] = useState(true);
     const [grimProb, setGrimProb] = useState("");
     const [perfectProb, setPerfectProb] = useState("");
+    const [critProb, setCritProb] = useState("");
     const [cutProb, setCutProb] = useState("");
     const [remapError, setRemapError] = useState<string | null>(null);
 
@@ -202,6 +242,9 @@ export default function RollsWithThornsAnalysis() {
     if (perfectProb != null) {
         remap["perfect"] = parseFraction(perfectProb);
     }
+    if (critProb != null) {
+        remap["critical"] = parseFraction(critProb);
+    }
     if (cutProb != null) {
         remap["cut"] = parseFraction(cutProb);
     }
@@ -211,7 +254,8 @@ export default function RollsWithThornsAnalysis() {
             Number(selectedNumDice),
             Number(selectedNumThorns),
             Number(selectedNumMastery),
-            remap
+            critsIgnoreThorns,
+            remap,
         ) || {
             disaster: 0,
             grim: 0,
@@ -227,6 +271,7 @@ export default function RollsWithThornsAnalysis() {
             Number(selectedNumDice),
             Number(selectedNumThorns),
             Number(selectedNumMastery),
+            critsIgnoreThorns,
             remap
         );
         if (p == null) {
@@ -235,7 +280,13 @@ export default function RollsWithThornsAnalysis() {
             setRemapError(null);
             setSelectedProportions(p);
         }
-    }, [JSON.stringify(remap), selectedNumDice, selectedNumThorns, selectedNumMastery]);
+    }, [
+        JSON.stringify(remap),
+        selectedNumDice,
+        selectedNumThorns,
+        selectedNumMastery,
+        critsIgnoreThorns,
+    ]);
 
     if (Number(selectedNumMastery) == 0 && _.isEmpty(remap)) {
         let totalAnalytical = 0;
@@ -280,13 +331,12 @@ export default function RollsWithThornsAnalysis() {
         selectedProportions,
         selectedRow
     );
-    const maxProportion = _.max(
-        Object.values(data)
-            .map((r) => Object.values(r))
-            .flat()
-            .flat()
-            .map((r) => r.proportion)
-    );
+    const plotData = Object.entries(selectedProportions).map(([r, p]) => ({
+        result: r.replace("_", " "),
+        proportion: p,
+        fill: barColors[r],
+    }));
+    const maxProportion = _.max(plotData.map((r) => r.proportion));
     return (
         <HeaderFooter
             title="Rolls with Thorns"
@@ -391,46 +441,75 @@ export default function RollsWithThornsAnalysis() {
                                 }
                                 label="5s count as 6s, 4s count as 1s"
                             />
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        checked={critsIgnoreThorns}
+                                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                            const checked = Boolean(e.target.checked);
+                                            setCritsIgnoreThorns(checked);
+                                        }}
+                                    />
+                                }
+                                label="Crits ignore thorns"
+                            />
                             {/* {remapError ? <Alert severity="error">{remapError}</Alert> : null} */}
-                            <TextField
-                                variant="outlined"
-                                value={grimProb}
-                                disabled={fivesAsSixesFoursAsOnes}
-                                onChange={(e) => {
-                                    const newValue = e.target.value;
-                                    setGrimProb(newValue);
-                                }}
-                                placeholder={`Probability to roll a Grim on a single die (default ${defaultGrim.toFixed(
-                                    2
-                                )}).`}
-                                sx={{ minWidth: "270px" }}
-                            ></TextField>
+                            <Stack spacing={2}>
 
-                            <TextField
-                                variant="outlined"
-                                value={perfectProb}
-                                disabled={fivesAsSixesFoursAsOnes}
-                                onChange={(e) => {
-                                    const newValue = e.target.value;
-                                    setPerfectProb(newValue);
-                                }}
-                                placeholder={`Probability to roll a Perfect on a single die (default ${defaultPerfect.toFixed(
-                                    2
-                                )}).`}
-                                sx={{ minWidth: "270px" }}
-                            ></TextField>
-                            <TextField
-                                variant="outlined"
-                                value={cutProb}
-                                onChange={(e) => {
-                                    const newValue = e.target.value;
-                                    setCutProb(newValue);
-                                }}
-                                placeholder={`Probability for a single thorn to cut a roll (default ${defaultCut.toFixed(
-                                    2
-                                )}).`}
-                                sx={{ minWidth: "270px" }}
-                            ></TextField>
+                                <TextField
+                                    variant="outlined"
+                                    value={grimProb}
+                                    disabled={fivesAsSixesFoursAsOnes}
+                                    onChange={(e) => {
+                                        const newValue = e.target.value;
+                                        setGrimProb(newValue);
+                                    }}
+                                    placeholder={`Probability to roll a Grim on a single die (default ${defaultGrim.toFixed(
+                                        2
+                                    )}).`}
+                                    sx={{ minWidth: "270px" }}
+                                ></TextField>
+
+                                <TextField
+                                    variant="outlined"
+                                    value={perfectProb}
+                                    disabled={fivesAsSixesFoursAsOnes}
+                                    onChange={(e) => {
+                                        const newValue = e.target.value;
+                                        setPerfectProb(newValue);
+                                    }}
+                                    placeholder={`Probability to roll a Perfect on a single die (default ${defaultPerfect.toFixed(
+                                        2
+                                    )}).`}
+                                    sx={{ minWidth: "270px" }}
+                                ></TextField>
+                                <MaterialTooltip title="The probability that you roll one of the results that, if you had rolled two, you'd get a Critical, i.e. the probability of rolling a 6 in default Moxie">
+                                    <TextField
+                                        variant="outlined"
+                                        value={critProb}
+                                        onChange={(e) => {
+                                            const newValue = e.target.value;
+                                            setCritProb(newValue);
+                                        }}
+                                        placeholder={`Probability to roll a Critical on a single die (default ${defaultCrit.toFixed(
+                                            2
+                                        )}).`}
+                                        sx={{ minWidth: "270px" }}
+                                    ></TextField>
+                                </MaterialTooltip>
+                                <TextField
+                                    variant="outlined"
+                                    value={cutProb}
+                                    onChange={(e) => {
+                                        const newValue = e.target.value;
+                                        setCutProb(newValue);
+                                    }}
+                                    placeholder={`Probability for a single thorn to cut a roll (default ${defaultCut.toFixed(
+                                        2
+                                    )}).`}
+                                    sx={{ minWidth: "270px" }}
+                                ></TextField>
+                            </Stack>
                         </FormGroup>
                     </DialogContent>
                 </Dialog>
@@ -463,7 +542,7 @@ export default function RollsWithThornsAnalysis() {
                                     domain={[
                                         0,
                                         maxProportion
-                                            ? Number(maxProportion.toPrecision(2))
+                                            ? Number(_.max([0.5, maxProportion])?.toPrecision(2))
                                             : "auto",
                                     ]}
                                 />
